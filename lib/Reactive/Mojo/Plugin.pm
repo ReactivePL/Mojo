7e229999dd00759ab0127466674bb868ce79d78a
Robert Moore <robert@r-moore.tech>
2025-06-20 17:39:10 +0100

    try again to include git repo details :p

--- lib/Reactive/Mojo/Plugin.pm
+++ lib/Reactive/Mojo/Plugin.pm
@@ -17,11 +17,11 @@ Reactive::Mojo::Plugin - Mojolicious plugin for Reactive
 
 =head1 VERSION
 
-Version 0.102
+Version 0.103
 
 =cut
 
-our $VERSION = '0.102';
+our $VERSION = '0.103';
 
 
 =head1 SYNOPSIS
